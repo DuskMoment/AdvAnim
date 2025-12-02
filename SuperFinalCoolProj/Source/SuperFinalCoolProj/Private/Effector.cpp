f980293ce67e52e90d6a65599d157df3c010a092
Tristan Schonfeldt-Aultman <tristan.schonfeldt-a@mymail.champlain.edu>
2025-12-02 09:49:10 -0500

    Added effector class.

---
+++ SuperFinalCoolProj/Source/SuperFinalCoolProj/Private/Effector.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Effector.h"
+
+// Sets default values
+AEffector::AEffector()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh)"));
+	SetRootComponent(mesh);
+}
+
+// Called when the game starts or when spawned
+void AEffector::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEffector::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
